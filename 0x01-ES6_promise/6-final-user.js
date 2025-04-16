15fe1c720f18c5e9849081f2f61e17bc33cef7eb
venopyX <gladsonchala@gmail.com>
2025-04-16 05:11:06 +0300

    fix: correct syntax in handleProfileSignup function for better readability

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,12 +5,12 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
   const result = [];
   return Promise.allSettled(promises).then((results) => {
-    results.map(({ status, value, reason }) =>
+    results.map(({ status, value, reason }) => (
       result.push({
         status,
         value: status === 'rejected' ? reason.toString() : value,
       })
-    );
+    ));
     return result;
   });
 }
