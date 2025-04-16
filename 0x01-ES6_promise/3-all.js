7cfd46c19ca8bf17127e8d3d654c3e639e580cb0
venopyX <gladsonchala@gmail.com>
2025-04-16 05:12:54 +0300

    fix: simplify console log syntax in handleProfileSignup function for better readability

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,9 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((value) => {
-      console.log(
-        `${value[0].body} ${value[1].firstName} ${value[1].lastName}`
-      );
+      console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }
