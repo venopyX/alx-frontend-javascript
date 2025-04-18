8221377cc0c34522365080521d4b9dde87946c49
venopyX <gladsonchala@gmail.com>
2025-04-18 21:35:31 +0300

    feat: add Airport class and main file to demonstrate its usage

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-underscore-dangle */
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get [Symbol.toStringTag]() {
+    return `${this._code}`;
+  }
+}
