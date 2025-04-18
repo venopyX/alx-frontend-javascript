bd77d00da53cbebb0a6b313a1f698871e0e07116
venopyX <gladsonchala@gmail.com>
2025-04-18 22:04:38 +0300

    refactor: reorganize class instance declarations for improved readability

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -14,8 +14,6 @@ export class ALXClass {
     return this._location;
   }
 }
-const class2019 = new ALXClass(2019, 'San Francisco');
-const class2020 = new ALXClass(2020, 'San Francisco');
 
 export class StudentALX {
   constructor(firstName, lastName, alxClass) {
@@ -36,12 +34,14 @@ export class StudentALX {
     return `${this._firstName} ${this._lastName} - ${this._alxClass.year} - ${this._alxClass.location}`;
   }
 }
+
+const class2019 = new ALXClass(2019, 'San Francisco');
+const class2020 = new ALXClass(2020, 'San Francisco');
+
 const student1 = new StudentALX('Guillaume', 'Salva', class2020);
 const student2 = new StudentALX('John', 'Doe', class2020);
 const student3 = new StudentALX('Albert', 'Clinton', class2019);
 const student4 = new StudentALX('Donald', 'Bush', class2019);
 const student5 = new StudentALX('Jason', 'Sandler', class2019);
 
-const listOfStudents = [student1, student2, student3, student4, student5];
-
-export default listOfStudents;
+export default [student1, student2, student3, student4, student5];
