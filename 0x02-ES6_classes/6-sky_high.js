4e196b8c6691a8f9106232e0cc9ff4ca19031709
venopyX <gladsonchala@gmail.com>
2025-04-18 21:32:49 +0300

    feat: add SkyHighBuilding class with methods for sqft and floors

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-underscore-dangle */
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  // Getter for floors
+  get floors() {
+    return this._floors;
+  }
+
+  // override method
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
